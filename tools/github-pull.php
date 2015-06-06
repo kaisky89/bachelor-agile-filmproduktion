5fa0481f0a369c830e539dfce42e542a060e7d96
kaisky89 <kaisky89@gmail.com>
2015-06-06 16:13:16 +0200

    testing the php exec commands...

--- tools/github-pull.php
+++ tools/github-pull.php
@@ -4,3 +4,9 @@ $output = shell_exec('git pull');
 echo "<pre>$output</pre>";
 
 ?>
+
+<?php
+// Gibt den Benutzernamen aus, unter dem der PHP/HTTPD-Prozess läuft
+// (auf einem System in dem das Programm "whoami" im Ausführungspfad liegt)
+echo exec('whoami');
+?>
