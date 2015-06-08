c641638cbf31d8067519d7685cd0d785fdda1775
kaisky89 <kaisky89@gmail.com>
2015-06-08 18:06:22 +0200

    Bug Fixes

--- tools/tex-header.php
+++ tools/tex-header.php
@@ -4,7 +4,6 @@ include_once 'createAllChaptersTex.php';
 
 `cp -R tex ../pipeline/04-texprocess`;
 
-`mkdir ../pipeline/04-texprocess/chapters`;
 `cp -R ../pipeline/03-post ../pipeline/04-texprocess/chapters`;
 
 createAllChaptersTex('../pipeline/04-texprocess/chapters', '../pipeline/04-texprocess/all-chapters.tex', 'chapters/');
