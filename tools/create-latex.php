3362b55c024eade1a536a0260f7603526bfb7cd1
kaisky89 <kaisky89@gmail.com>
2015-06-08 12:40:31 +0200

    adding pre processor

--- tools/create-latex.php
+++ tools/create-latex.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once 'processor.php';
+
 # Ordner latex aufräumen
 
 `rm -R ../latex`;
@@ -9,12 +11,19 @@
 
 # Alle Kapitel einzeln in latex konvertieren
 
+$markdownPreProcessor = new WordProcessor('rules/markdown-pre.json');
+
 $files = scandir('../chapters/');
 
 foreach ($files as $file) {
   if ($file == "." || $file == "..") {
     continue;
   }
+
+  $markdownPreProcessor->process($file, $file);
+
   $texfile = explode('.', $file)[0].'.tex';
+
   `/usr/bin/pandoc -f markdown --latex-engine=xelatex -R -i ../chapters/$file  -o ../latex/$texfile`; 
 }
+
---
+++ tools/processor.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+* 
+*/
+class Rule
+{
+  public $regex;
+  public $replace;
+
+  public function __construct($regex, $replace)
+  {
+    $this->regex = $regex;
+    $this->replace = $replace;
+  }
+}
+
+/**
+* 
+*/
+class WordProcessor
+{
+  private $rules[];
+
+  public function __construct($rulesFile)
+  {
+    $this->rules = openJsonFile($rulesFile)
+  }
+
+  public function processFile($from, $to)
+  {
+    $string = openFile($from);
+    $string = process($string);
+    saveFile($string, $to);
+  }
+
+  private function openFile($from)
+  {
+    return file_get_contents($from);
+  }
+
+  private function openJsonFile($from)
+  {
+    $string = $openFile($from);
+    return json_decode($string);
+  }
+
+  private function saveFile($string, $to)
+  {
+    file_put_contents($to, $string);
+  }
+
+  private function process($string)
+  {
+    foreach ($this->rules as $rule) {
+      $string = ereg_replace(, "", $string);
+    }
+  }
+}
