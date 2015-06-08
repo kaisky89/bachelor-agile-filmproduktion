6c94ba07013f5331a4ecdb7e45c7310fe223da29
kaisky89 <kaisky89@gmail.com>
2015-06-08 17:52:43 +0200

    Bug Fixes

--- tools/tex-header.php
+++ tools/tex-header.php
@@ -2,10 +2,10 @@
 
 include_once 'createAllChaptersTex.php';
 
-`cp -R tex ../pipeline/04-texprocess`:
+`cp -R tex ../pipeline/04-texprocess`;
 
 `mkdir ../pipeline/04-texprocess/chapters`;
-`cp -R ../pipeline/03-post ../pipeline/04-texprocess/chapters`:
+`cp -R ../pipeline/03-post ../pipeline/04-texprocess/chapters`;
 
 createAllChaptersTex('../pipeline/04-texprocess/chapters', '../pipeline/04-texprocess/all-chapter.tex', 'chapters/');
 
