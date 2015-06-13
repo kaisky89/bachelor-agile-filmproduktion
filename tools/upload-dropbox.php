97636e06b225a5eb0cf68ef296320616ea0d4f0e
kaisky89 <kaisky89@gmail.com>
2015-06-13 20:52:20 +0200

    zusätzliche Sync über Bittorrent Sync

--- tools/upload-dropbox.php
+++ tools/upload-dropbox.php
@@ -3,5 +3,7 @@
 
 echo `./Dropbox-Uploader/dropbox_uploader.sh -f /etc/dropbox_uploader upload ../pipeline/04-texprocess/Bachelorarbeit-Kai-Wittmann.pdf /`;
 
+`cp ../pipeline/04-texprocess/Bachelorarbeit-Kai-Wittmann.pdf /var/www/btsync.heart-co.de/Temp/`;
+
 ?>
 </pre>
