59654e4172cf2b5317b483d35ea584cf0c9548c3
kaisky89 <kaisky89@gmail.com>
2015-06-13 22:28:18 +0200

    dropbox upload deaktivieren

--- tools/upload-dropbox.php
+++ tools/upload-dropbox.php
@@ -1,7 +1,7 @@
 <pre>
 <?php
 
-echo `./Dropbox-Uploader/dropbox_uploader.sh -f /etc/dropbox_uploader upload ../pipeline/04-texprocess/Bachelorarbeit-Kai-Wittmann.pdf /`;
+// echo `./Dropbox-Uploader/dropbox_uploader.sh -f /etc/dropbox_uploader upload ../pipeline/04-texprocess/Bachelorarbeit-Kai-Wittmann.pdf /`;
 
 // `cp ../pipeline/04-texprocess/Bachelorarbeit-Kai-Wittmann.pdf /var/www/btsync.heart-co.de/Temp/`;
 
