c0afaf0785b079d1f7929f98aff9c25f69e6eaec
kaisky89 <kaisky89@gmail.com>
2015-06-13 19:39:28 +0200

    automatisiert in die Dropbox speichern

--- tools/upload-dropbox.php
+++ tools/upload-dropbox.php
@@ -1,7 +1,7 @@
 <pre>
 <?php
 
-echo "uploading to Dropbox..."
+echo `./Dropbox-Uploader/dropbox_uploader.sh -f /etc/dropbox_uploader upload ../pipeline/04-texprocess/Bachelorarbeit-Kai-Wittmann.pdf /`;
 
 ?>
 </pre>
